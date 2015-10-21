0cb30e35d3f9bafb6de3bd5ff6754a05cb4990a1
karthik-node <karthik.node@gmail.com>
2015-10-21 22:29:41 +0530

    fb auth commited

---
+++ config/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1081491161885540',
+		'clientSecret': '68d379cd618dd2abddf918f1d6cfc972',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	}
+
+
+}
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+	'uri'	: 'mongodb://nani:root@ds041404.mongolab.com:41404/mydb'
+}
